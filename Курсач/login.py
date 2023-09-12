7bda95ab25b61e80e2ad74605182f97a61316420
ChazGrant <ilya20012209@yandex.ru>
2023-09-12 19:14:16 +0300

    Started doing adding function

--- Курсач/login.py
+++ Курсач/login.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtWidgets
 from login_formUI import Ui_MainWindow
 import psycopg2
-
+from main import MainWindow
 
 class LoginForm(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -17,28 +17,36 @@ class LoginForm(QtWidgets.QMainWindow, Ui_MainWindow):
         if not (username or password):
             return
         
-        CONFIG = {
-            "database": "ambulance",
-            "user": username,
-            "password": password,
-            "host": "127.0.0.1",
-            "port": 5432
-        }
+        from CONFIG import CONFIG
 
-        conn = psycopg2.connect(**CONFIG)
-        cursor = conn.cursor()
+        # ambulance_operator (fN6Pn!5
+        # doctor u8YVX,:2
+        # nurse ]Lg4SSr4
+        CONFIG["user"] = "postgres"
+        CONFIG["password"] = "postgres"
 
-        queries = ["SELECT * FROM call_reason LIMIT 10",
-                    "SELECT * FROM sick_people LIMIT 10",
-                    "SELECT * FROM call_requests LIMIT 10"]
+        print(CONFIG)
+        try:
+            conn = psycopg2.connect(**CONFIG)
+            cursor = conn.cursor()
+        except psycopg2.OperationalError:
+            return
 
-        for query in queries:
-            try:
-                cursor.execute(query)
-                print(cursor.fetchall())
-            except Exception as e:
-                conn.rollback()
-                print(str(e))
+        self.widget = MainWindow(cursor, conn)
+        self.widget.show()
+        self.close()
+
+        # queries = ["SELECT * FROM call_reason LIMIT 10",
+        #             "SELECT * FROM sick_people LIMIT 10",
+        #             "SELECT * FROM call_requests LIMIT 10"]
+
+        # for query in queries:
+        #     try:
+        #         cursor.execute(query)
+        #         print(cursor.fetchall())
+        #     except Exception as e:
+        #         conn.rollback()
+        #         print(str(e))
 
 
 if __name__ == "__main__":
--- Курсач/main.py
+++ Курсач/main.py
@@ -36,41 +36,110 @@
 # просматривать таблицу больные)
 
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from main_formUI import Ui_MainWindow
+import psycopg2
 
 
 TABLES_DICT = {
     "Больные": "sick_people",
     "Заявки на вызов": "call_requests",
-    "Причины вызова": "call_reason"
+    "Причины вызова": "call_reason",
+    "Станции скорой помощи": "first_aid_stations",
+    "Процедуры": "procedure",
+    "Заявки на процедуры": "procedure_application",
+    "Социальные статусы": "social_status"
 }
 
-class MainWindows(QtWidgets.QMainWindow, Ui_MainWindow):
-    def __init__(self, cursor):
-        super(MainWindows, self).__init__()
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, cursor, conn):
+        super(MainWindow, self).__init__()
         self.setupUi(self)
 
         self.__setComboBoxes()
         self._cursor = cursor
+        self._conn = conn
+
+        self._new_rows_added = list()
 
         self.visualiseTable_pushButton.pressed.connect(self._visualizeTable)
         self.visualizeQuery_pushButton.pressed.connect(self._visualizeQuery)
+        self.addRow_pushButton.pressed.connect(self._addRowToTheTableWidget)
+        self.addRecord_pushButton.pressed.connect(self._addRecord)
 
     def __setComboBoxes(self):
-        for table in TABLES_DICT.keys():
-            self.tables_comboBox.addItem(table)
+        self.__setTables()
+        self.__setQueries()
 
     def __setTables(self):
-        ...
+        for table in TABLES_DICT.keys():
+            self.tables_comboBox.addItem(table)
 
     def __setQueries(self):
         ...
 
+    def _addRowToTheTableWidget(self):
+        current_row_amount = self.tableWidget.rowCount()
+        if current_row_amount == 0:
+            return
+            
+        
+        self.tableWidget.setRowCount(current_row_amount + 1)
+        self._new_rows_added.append(current_row_amount + 1)
+
+    def _addRecord(self):
+        columns_names = []
+        table_name = TABLES_DICT[self.tables_comboBox.currentText()]
+        for column in range(self.tableWidget.columnCount()):
+            columns_names.append(self.tableWidget.horizontalHeaderItem(column).\
+                                 text())
+
+        for row_idx in self._new_rows_added:
+            row = []
+            print("CURRENT ROW: ", row_idx)
+            for column_idx in range(1, self.tableWidget.columnCount()):
+                error_occured = False
+                print("CURRENT COLUMN: ", column_idx)
+                try:
+                    row.append(self.tableWidget.item(row_idx - 1, column_idx).text())
+                except AttributeError:
+                    error_occured = True
+                    break
+
+            if error_occured:
+                continue
+
+            table_columns = ", ".join(columns_names[1:])
+            table_values = ", ".join(row)
+            query = "INSERT INTO %s(%s) VALUES (%s)" % (table_name, table_columns, table_values)
+            self._cursor.execute(query)
+            self._conn.rollback()
+            print(query)
+
+        
+
     def _visualizeTable(self):
-        self._cursor.execute("SELECT * FROM %s" % TABLES_DICT[self.tables_comboBox.currentText()])
-        colnames = [desc[0] for desc in self._cursor.description]
-        print(colnames)
+        try:
+            self._cursor.execute("SELECT * FROM %s" % TABLES_DICT[self.tables_comboBox.currentText()])
+        except psycopg2.errors.InsufficientPrivilege:
+            print("Беды с правами")
+            self._conn.rollback()
+            return
+        columns_names = [desc[0] for desc in self._cursor.description]
+        columns_amount = len(columns_names)
+        
+        self.tableWidget.setColumnCount(columns_amount)
+        self.tableWidget.setHorizontalHeaderLabels(columns_names)
+
+        data = self._cursor.fetchall()
+        self.tableWidget.setRowCount(len(data))
+        for row_idx, row in enumerate(data):
+            for column_idx, item in enumerate(row):
+                _item = QtWidgets.QTableWidgetItem(str(item))
+                _item.setFlags(QtCore.Qt.ItemIsEnabled)
+                self.tableWidget.setItem(row_idx, column_idx, _item)
+
+        self.tableWidget.resizeColumnsToContents()
 
     def _visualizeQuery(self):
         ...
@@ -78,6 +147,6 @@ class MainWindows(QtWidgets.QMainWindow, Ui_MainWindow):
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
-    widget = MainWindows()
+    widget = MainWindow()
     widget.show()
     app.exec_()
